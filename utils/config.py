424ebbef48f0564b572a8ceb70b98a1d74da99a0
sy1-bl <120222565+sy1-bl@users.noreply.github.com>
2022-12-09 21:37:14 +0000

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -2,10 +2,10 @@
 TIMEOUT = 90  # refresh page every 90s
 
 # =============== VISA CENTER SELECTION ===============
-CENTER = ('England', 'Manchester', 'Normal', 'Tourism')
-# CENTER = ('Scotland', 'Edinburgh', 'Normal', 'Tourism')
-# CENTER = ('Bristol', 'London', 'Normal', 'Tourist')
+CENTER = ('Edinburgh', 'Normal', 'Tourism')
+# CENTER = ('Scotland', 'Edinburgh', 'Normal', 'Tourist')
+
 # =============== PERSONAL CONFIG FOR VISA ===============
 OPENED_PAGE = 'https://uk.blsspainvisa.com/visa4spain/book-date/QwertYU123'  # your BLS appointment page link
-EMAIL = '123@456.com'  # BLS email
-PASSWORD = 'BLSpassword'  # BLS password
+EMAIL = 'vivianas214@outlook.com'  # BLS email
+PASSWORD = 'Shiying214@'  # BLS password
